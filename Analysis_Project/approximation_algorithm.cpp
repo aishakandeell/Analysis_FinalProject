3a36b3a291f9978187dbbe794e5a9335908791f4
aishakandeell <aishakandeel@aucegypt.edu>
2025-05-20 22:50:56 +0300

    approx is working

--- Analysis_Project/approximation_algorithm.cpp
+++ Analysis_Project/approximation_algorithm.cpp
@@ -14,6 +14,7 @@ int compute_finish_time(const vector<int>& durations, int k) {
 
 vector<int> schedule_inspections(const vector<vector<int>>& durations, int L, int C, int T) {
     vector<int> inspection_times;
+    set<int> used_times;
 
     if (C == 1) {
         int max_finish = 0;
@@ -26,7 +27,13 @@ vector<int> schedule_inspections(const vector<vector<int>>& durations, int L, in
         return inspection_times;
     }
 
+    int max_students = 0;
+    for (const auto& lab : durations) {
+        max_students = max(max_students, (int)lab.size());
+    }
+
     double standard_interval = static_cast<double>(T) / (C + 1);
+
     for (int i = 1; i <= C; ++i) {
         double target_time = i * standard_interval;
         vector<int> candidate_times;
@@ -38,18 +45,29 @@ vector<int> schedule_inspections(const vector<vector<int>>& durations, int L, in
             }
         }
 
-        if (candidate_times.empty()) continue;
+        // Remove duplicates
+        candidate_times.erase(remove_if(candidate_times.begin(), candidate_times.end(),
+            [&](int t) { return used_times.count(t); }), candidate_times.end());
 
-        int closest_time = candidate_times[0];
-        int min_diff = abs(candidate_times[0] - target_time);
-        for (int t : candidate_times) {
-            int diff = abs(t - target_time);
-            if (diff < min_diff || (diff == min_diff && t < closest_time)) {
-                min_diff = diff;
-                closest_time = t;
+        // Select the closest candidate to target_time
+        int chosen_time;
+        if (!candidate_times.empty()) {
+            chosen_time = candidate_times[0];
+            int min_diff = abs(candidate_times[0] - target_time);
+            for (int t : candidate_times) {
+                int diff = abs(t - target_time);
+                if (diff < min_diff || (diff == min_diff && t < chosen_time)) {
+                    min_diff = diff;
+                    chosen_time = t;
+                }
             }
         }
-        inspection_times.push_back(closest_time);
+        else {
+            chosen_time = static_cast<int>(round(target_time));
+        }
+
+        inspection_times.push_back(chosen_time);
+        used_times.insert(chosen_time);
     }
     return inspection_times;
 }
@@ -58,10 +76,13 @@ int calculate_total_unoccupied_time(int T, int L, const vector<vector<int>>& dur
     int total_unoccupied = 0;
     for (int i = 0; i < inspection_times.size(); ++i) {
         int inspect_time = inspection_times[i];
+        cout << "inspection " << i<<", ";
         for (int l = 0; l < L; ++l) {
             if ((int)durations[l].size() > i) {
-                int finish_time = compute_finish_time(durations[l], i);
-                total_unoccupied += max(0, inspect_time - finish_time);
+                cout << "lab " << l << " :";
+                int finish_time = compute_finish_time(durations[l], i+1);
+                cout << "finish time: " << finish_time<<endl;
+                total_unoccupied += inspect_time - finish_time;
             }
         }
     }
@@ -69,7 +90,7 @@ int calculate_total_unoccupied_time(int T, int L, const vector<vector<int>>& dur
 }
 
 int main() {
-    int L = 3, C = 3, D = 1;
+    int L = 3, C = 2, D = 1;
     int T = D * 24;
 
     vector<vector<int>> durations = {
